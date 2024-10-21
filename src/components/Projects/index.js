548115d5816e0e0fd56e53e41d3302571b24f850
Jibru-ski <barakajibran2004@gmail.com>
2024-10-21 17:39:28 +0300

    Prpjects page

---
+++ src/components/Projects/index.js
@@ -0,0 +1,72 @@
+import './index.scss'
+import * as projects from '../../assets/projects-imgs';
+
+const Projects = () => {
+    return(
+        <>
+         <section class="project-hero">
+        <h1>Projects</h1>
+        <p>My remarkable projects with memorable experiences.</p>
+        <div class="project-wrapper">
+            <a href="#" class="prj-container">
+                <div class="prj-img">
+                    <img class="prj-image" src={projects.project1} alt=""></img>
+                </div>
+                <div class="prj-details">
+                    <div>
+                        <h3>Hugs</h3>
+                        <p>E-commerce</p>
+                    </div>
+                    <div class="prj-date">
+                        <span>2023</span>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="prj-container">
+                <div class="prj-img">
+                    <img class="prj-image" src={projects.project2} alt=""></img>
+                </div>
+                <div class="prj-details">
+                    <div>
+                        <h3>Shadxw</h3>
+                        <p>E-commerce</p>
+                    </div>
+                    <div class="prj-date">
+                        <span>2023</span>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="prj-container">
+                <div class="prj-img">
+                    <img class="prj-image" src={projects.project3} alt=""></img>
+                </div>
+                <div class="prj-details">
+                    <div>
+                        <h3>Desain</h3>
+                        <p>Portfolio</p>
+                    </div>
+                    <div class="prj-date">
+                        <span>2023</span>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="prj-container">
+                <div class="prj-img">
+                    <img class="prj-image" src={projects.project4} alt=""></img>
+                </div>
+                <div class="prj-details">
+                    <div>
+                        <h3>Drip</h3>
+                        <p>Watch App</p>
+                    </div>
+                    <div class="prj-date">
+                        <span>2023</span>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </section></>
+    );
+}
+
+export default Projects
